8b5db01d2a2281986cba9d85acd619e83aa984c4
Phillip Bogopane <pm.bogopane@gamil.com>
2024-03-22 14:28:06 +0200

    commit

--- Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
+++ Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
@@ -4,31 +4,41 @@
 // Given a variable that holds a user's preferred type of Monster Energy drink, 
 // print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
+// #### Challenge 1: Flavor Selection : Solution.
+
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-console.log(``);
+console.log(`Preferred Flavor: ${userPreference === 'Sugar-free' ? 'Sugar-free' : 'Regular'}`);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
+// #### Challenge 2: Stock Check : Solution.
+
 let cansLeft = 3; // Any number of cans
-console.log(``);
+console.log(`${cansLeft < 5 ? 'Time to restock!' : 'We\'re stocked!'}`);
+
 
 
 // #### Challenge 3: Workout Intensity
 // Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout. 
 // If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
 
+// #### Challenge 3: Solution
+
 let heartRate = 95; // Current heart rate in bpm
-console.log(`'}`);
+console.log(`${heartRate < 100 ? 'Boost needed!' : 'Energy levels are high!'}`);
+
 
 
 // #### Challenge 4: Temperature Suitability
 // Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
+// #### Challenge 4: Temperature Suitability : Solutions
+
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
+console.log(`${currentTemp <= 5 ? 'Chilled to perfection!' : 'Needs a cooler!'}`);
 
 
 // #### Challenge 5: Late Night Coding Session
